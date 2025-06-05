676a9678415f0f792c18ae5292fc9add625977e4
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-04 22:35:23 -0500

    Prueba filtros lower, upper, length, date, add y default en plantilla

--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
+
 def myHomeView(request, *args, **kwargs):
     myContext = {
         'myText': 'Esto es sobre nosotros',
@@ -10,3 +12,12 @@ def myHomeView(request, *args, **kwargs):
     return render(request, 'home.html', myContext) #Contexto
 def anotherView(request):
     return HttpResponse("<h1>Solo otra pagina</h1>")
+
+def filtros_template(request):
+    return render(request, 'filtros.html', {
+        'nombre': 'Cristhian',
+        'lista': ['A', 'B', 'C'],
+        'numero': 10,
+        'fecha': datetime.now(),
+        'vacia': ''
+    })
