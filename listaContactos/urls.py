3b0e6fbdbc4baffcadc0e337a6c288c0094989e5
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-05 19:18:53 -0500

    Formulario sencillo para el app Personas: formulario y vista para crear una nueva persona

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -19,10 +19,13 @@ from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
 from personas.views import personaTestView
+from inicio.views import anotherView
+from personas.views import personaTestView, personaCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myHomeView, name='home'),
     path('another/', anotherView, name='otro'),
-    path('persona/', personaTestView, name='otro'),
+    path('persona/', personaTestView, name='testViewPersona'),
+    path('agrega/', personaCreateView, name='createPersona'),
 ]
